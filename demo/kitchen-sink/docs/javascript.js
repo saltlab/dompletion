ccc8a17bcce995eac4e1caeb5fc4cb1c7ce21a0c
Kartik Bajaj <kbajaj@Kartiks-MacBook-Pro.local>
2013-11-27 00:42:23 -0800

    commit with dompletion code

--- demo/kitchen-sink/docs/javascript.js
+++ demo/kitchen-sink/docs/javascript.js
@@ -1,5 +1,2 @@
-function foo(items, nada) {
-    for (var i=0; i<items.length; i++) {
-        alert(items[i] + "juhu\n");
-    }	// Real Tab.
-}
+var a = document.getElementsByTagName("div");
+b = a.getElementById("
